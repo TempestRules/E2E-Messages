ab796a317dfc2fea7aa312a9174ac4be1952d87f
Victor <43045051+TempestRules@users.noreply.github.com>
2024-05-29 21:01:34 +0200

    Login page
    Design completed.

--- E2E-Messaging-App/src/app/login/login/login.component.ts
+++ E2E-Messaging-App/src/app/login/login/login.component.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormControl, FormGroup } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatRadioModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  
+  public hidePassword = true;
 
+  public loginForm = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  public clickEvent(event: MouseEvent) {
+    this.hidePassword = !this.hidePassword;
+    event.stopPropagation();
+  }
+
+  public handleSubmit() {
+    alert(this.loginForm.value.username + ' | ' + this.loginForm.value.password);
+  }
 }
